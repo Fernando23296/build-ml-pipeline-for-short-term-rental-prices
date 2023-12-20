1596b3ebaf167d17503790315765cd605f5c00c1
fernando23296 <fernando23296@gmail.com>
2023-12-20 19:30:15 +0100

    Adding code that  drop rows in the dataset that are not in the proper geolocation

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -6,6 +6,7 @@ import argparse
 import logging
 import wandb
 import pandas as pd
+import numpy as np
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
 logger = logging.getLogger()
@@ -26,11 +27,15 @@ def go(args):
     # Drop outliers
     min_price = args.min_price
     max_price = args.max_price
+
     idx = df['price'].between(min_price, max_price)
     df = df[idx].copy()
-    # Convert last_review to datetime
+
     df['last_review'] = pd.to_datetime(df['last_review'])
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
     df.to_csv("clean_sample.csv", index=False)
+
     artifact = wandb.Artifact(
      args.output_artifact,
      type=args.output_type,
